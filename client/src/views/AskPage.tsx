63ee83564daadd4cf1704e7a93dfa4620e473975
donbasta <faddilafarras@gmail.com>
2021-06-17 03:33:20 +0700

    add server host address

--- client/src/views/AskPage.tsx
+++ client/src/views/AskPage.tsx
@@ -2,10 +2,13 @@ import { io } from "socket.io-client";
 
 import QuestionForm from "../components/QuestionForm";
 
+const SOCKET_URL = "http://localhost:3001";
+
 const AskPage = () => {
-  const socket = io();
+  const socket = io(SOCKET_URL);
 
   const handleSubmit = () => {
+    console.log("test ke handleSubmit");
     socket.emit('askQuestion', {
       content: "test hard code message",
       sender: "donbasta"
