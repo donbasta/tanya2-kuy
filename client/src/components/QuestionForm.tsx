26727d5b14ccebc0a6d36a7dede64d6658dc515e
donbasta <faddilafarras@gmail.com>
2021-06-17 09:10:03 +0700

    feature: not using hardcoded question anymore

--- client/src/components/QuestionForm.tsx
+++ client/src/components/QuestionForm.tsx
@@ -1,14 +1,32 @@
-import { FC } from 'react';
+import { useState, FC, ChangeEvent } from 'react';
+
+interface IQuestion {
+  sender: string;
+  content: string;
+}
 
 interface ISubmitForm {
-  onSubmit: () => void;
+  onSubmit: (arg: IQuestion) => void;
 }
 
 const QuestionForm: FC<ISubmitForm> = ({ onSubmit }) => {
+
+  const [sender, setSender] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+
+  const handleSenderChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSender(e.target.value);
+  }
+
+  const handleContentChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setContent(e.target.value);
+  }
+
   return (
     <div>
-      <input className="input-message" autoComplete="off" />
-      <button onClick={onSubmit}>Submit</button>
+      <input className="input-message" autoComplete="off" onChange={handleSenderChange} />
+      <input className="input-message" autoComplete="off" onChange={handleContentChange}/>
+      <button onClick={() => onSubmit({sender, content})}>Submit</button>
     </div>
   )
 }
--- client/src/views/AskPage.tsx
+++ client/src/views/AskPage.tsx
@@ -2,14 +2,19 @@ import { io } from "socket.io-client";
 
 import QuestionForm from "../components/QuestionForm";
 
+interface IQuestion {
+  content: string;
+  sender: string;
+}
+
 const AskPage = () => {
   const socket = io(process.env.REACT_APP_BACKEND as any);
 
-  const handleSubmit = () => {
+  const handleSubmit = ({content, sender}: IQuestion) => {
     console.log("test ke handleSubmit");
     socket.emit('askQuestion', {
-      content: "test hard code message",
-      sender: "donbasta"
+      content: content,
+      sender: sender
     })
   }
 
