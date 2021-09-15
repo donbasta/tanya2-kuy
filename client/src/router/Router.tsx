5ad1f0edf61ef8cbecef570ddc9b4520d9db9a11
Farras Faddila <35418377+donbasta@users.noreply.github.com>
2021-09-15 21:01:18 +0700

    Feature/client (#2)
    * init server
    
    * add some file in client

--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import HostPage from '../views/HostPage';
 import AskPage from '../views/AskPage';
+import ManagePage from '../views/ManagePage';
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <Switch>
         <Route path='/host' component={HostPage}/>
         <Route path='/ask' component={AskPage}/>
+        <Route path="/manage" component={ManagePage}/>
       </Switch>
     </BrowserRouter>
   );
